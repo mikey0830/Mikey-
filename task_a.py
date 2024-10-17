702461209b6c4bad603f7cad041df4970874c23a
mikey0830 <lpgb8515@leeds.ac.uk>
2024-10-17 18:58:24 +0000

    Add task_a.py

--- task_a.py
+++ task_a.py
@@ -1,6 +1,13 @@
-day = input("Please enter a day of week: ").strip().capitalize()
-days = {"Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6, "Sunday": 7}
-if days in days:
-    print(f"{day} is day {days[day]}")
-else:
-    print("Please enter a valid day")
+def get_day_number():
+    days = {"monday": 1,"tuesday": 2,"wednesday": 3,"thursday": 4,"friday": 5,"saturday": 6,"sunday": 7}
+
+    day_input = input("Please enter a day of the week: ").strip().lower()
+
+    if day_input in days:
+        day_number = days[day_input]
+        formatted_day = day_input.capitalize()
+        print(f"{formatted_day} is day {day_number}")
+    else:
+        print("Please enter a valid day of the week.")
+
+get_day_number()
