0fff04bb55d302a75fec0b0b0fa9b3a7e849a41f
mikey0830 <lpgb8515@leeds.ac.uk>
2024-10-17 19:00:11 +0000

    Add task_c.py

--- task_c.py
+++ task_c.py
@@ -1,8 +1,16 @@
-def password_check():
+def password_validator():
     password = input("Please enter a password: ").strip()
+
     if len(password) < 8:
-        print("Your password must contain at least 8 characters")
-    elif password.isalpha() or password.isdigit():
-        print("Your password must contain a mix of letters and numbers")
-    else:
+        print("Your password must contain at least 8 characters, and a mix of letters and numbers")
+        return
+
+    has_letter = any(char.isalpha() for char in password)
+    has_number = any(char.isdigit() for char in password)
+
+    if has_letter and has_number:
         print("Your password is valid")
+    else:
+        print("Your password must contain at least 8 characters, and a mix of letters and numbers")
+
+password_validator()
